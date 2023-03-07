9bdf65dbfb99f52936f34908e3b1121c9934a914
GiuliaSaporiti <127093059+GiuliaSaporiti@users.noreply.github.com>
2023-03-07 16:57:28 +0100

    Add files via upload

--- Program.cs
+++ Program.cs
@@ -1,111 +1,46 @@
-﻿namespace pippofranco
+﻿namespace esercizio_2
 {
     internal class Program
     {
-        class Automobile
-        {
-            public decimal Prezzo { get; set; }
-            public string Marca { get; set; }
-            public string Modello { get; set; }
-
-            public int Anno { get; set; }
-
-            public void stampa() 
-            {
-                Console.WriteLine("automobile : {0} {1}, Anno: {2}, Prezzo:{3}  £", Marca, Modello, Anno, Prezzo);
-            }
-        }
-        class Animale
-        {
-            public string Specie { get; set; }
-            public string Habitat { get; set; }
-            public int Zampe { get; set; }
-            public string Colore { get; set; }
-
-            public void stampa()
-            {
-                Console.WriteLine($"animale di specie {Specie} che vive {Habitat}, ha {Zampe} zampe ed ha il manto {Colore}");
-            }
-        }
-        class Dipendente
+        class Persona
         {
             public string Nome { get; set; }
             public string Cognome { get; set; }
             public int Eta { get; set; }
 
-            public void stampa()
+            public void GetName()
+            
             {
-                Console.WriteLine($"sono {Nome} {Cognome} e ho {Eta} anni");
+                Console.WriteLine($"salve sono {Nome} ");
+            }
+            public void GetCognome()
+            {
+                Console.WriteLine($" il mio cognome è {Cognome}");
+            }
+
+            public void GetEta()
+            {
+                Console.WriteLine($"la mia età è di {Eta}");
+            }
+
+            public void GetDettagli()
+            {
+                Console.WriteLine($"sono molto contento di conoscervi sono {Nome} {Cognome} e ho {Eta} anni ");
             }
         }
-        class Atleta
-        {
-            public string Nome { get; set; }
-            public string Sport { get; set; }
-            public int Altezza { get; set; }
-            public int Peso { get; set; }
-
-            public void presentazione()
-            {
-                Console.WriteLine($"sono {Nome} pratico il {Sport} sono alto {Altezza} centimetri e peso {Peso} kg");
-            }
-        }
-
         static void Main(string[] args)
         {
-            Atleta duck = new Atleta();
-            duck.Nome = "duck";
-            duck.Sport = "nuoto";
-            duck.Altezza = 2;
-            duck.Peso = 50;
-            duck.presentazione();
-            Atleta bird = new Atleta();
-            bird.Nome = "bird";
-            bird.Sport = "danza";
-            bird.Altezza = 1;
-            bird.Peso = 36;
-            bird.presentazione();
+            Persona bella = new Persona();
+            bella.Nome = "Giulia";
+            bella.Cognome = "Sapo";
+            bella.Eta = 25;
 
-            Dipendente dipendente = new Dipendente();
-            dipendente.Nome = "severo";
-            dipendente.Cognome = "tuorlo";
-            dipendente.Eta = 67;
-            dipendente.stampa();
-            Dipendente dipendente2 = new Dipendente();
-            dipendente2.Nome = "carlotta";
-            dipendente2.Cognome = "parola";
-            dipendente2.Eta = 45;
-            dipendente2.stampa();
+            bella.GetName();
+            bella.GetCognome();
+            bella.GetEta();
+            bella.GetDettagli();
 
-            Animale tigre = new Animale();
-            tigre.Specie = "felino";
-            tigre.Habitat = "malesia";
-            tigre.Zampe = 4;
-            tigre.Colore = "arancione";
-            tigre.stampa();
-            Animale leone = new Animale();
-            leone.Specie = "felino";
-            leone.Habitat = "foresta";
-            leone.Zampe = 4;
-            leone.Colore = "giallo";
-            leone.stampa();
-
-            Automobile bmw = new Automobile();
-            bmw.Anno = 2010;
-            bmw.Marca = "BMW";
-            bmw.Modello = "x5";
-            bmw.Prezzo = 37000;
-            bmw.stampa();
-            Automobile tucson = new Automobile();
-            tucson.Anno = 2018;
-            tucson.Marca = "hunday";
-            tucson.Modello = "tucson";
-            tucson.Prezzo = 26000;
-            tucson.stampa();
-
-
+            
         }
-
-       
     }
 }
