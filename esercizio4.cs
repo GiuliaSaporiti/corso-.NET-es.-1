d558034dc8fff45a7a441cab7caf4975f68fc5f8
GiuliaSaporiti <127093059+GiuliaSaporiti@users.noreply.github.com>
2023-03-10 00:46:46 +0100

    Add files via upload

---
+++ esercizio4.cs
@@ -0,0 +1,35 @@
+﻿using System.ComponentModel.Design;
+
+namespace esercizio4
+{
+    internal class esercizio4
+    {
+        static void Main(string[] args)
+        {
+             string[] nomi = new string[] { "marta", "giulia", "gloria", "aiello" };
+             static void ricercaNome(string[] arr)
+             {
+                Console.WriteLine("nome cercato");
+                string nomeCercato = Console.ReadLine();
+                bool m = false;
+
+                foreach (string s in arr)
+                {
+                    if (s == nomeCercato)
+                    {
+                        m = true;
+                    }
+                }
+                if (m)
+                {
+                    Console.WriteLine("il nome esiste");
+                }
+                else
+                {
+                    Console.WriteLine("il nome non esiste nell'array");
+                }
+            }
+          ricercaNome(nomi);
+        }
+    }
+}
