d0a0b182e05bf1fe695e5e90cd7e708010f77c7d
GiuliaSaporiti <127093059+GiuliaSaporiti@users.noreply.github.com>
2023-03-10 00:42:14 +0100

    Add files via upload

---
+++ esercizio5.cs
@@ -0,0 +1,30 @@
+﻿using System.Globalization;
+
+namespace esercizio5
+{
+    internal class esercizio5
+    {   
+        static void Operazione(int[] arr)
+        {
+            int sum = 0;
+
+            foreach (int j in arr)
+            {
+                sum += j;
+            }
+
+            Console.WriteLine(sum);
+
+            double media = sum / arr.Length;
+
+            Console.WriteLine(media);
+        }
+
+        static void Main(string[] args)
+        {
+            int[] numbers = { 6, 5, 4, 3, 2, 1 };
+
+            Operazione(numbers);
+        }
+    }
+}
