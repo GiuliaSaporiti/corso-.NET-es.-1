9b887ee36128b8fd73d10172ec7a06440ed0ead1
GiuliaSaporiti <127093059+GiuliaSaporiti@users.noreply.github.com>
2023-03-17 18:47:40 +0100

    Add files via upload

--- Program.cs
+++ Program.cs
@@ -1,46 +1,19 @@
-﻿namespace esercizio_2
+﻿using System.Collections;
+
+namespace ciclo_for_arraylist
 {
     internal class Program
     {
-        class Persona
-        {
-            public string Nome { get; set; }
-            public string Cognome { get; set; }
-            public int Eta { get; set; }
+         public static void Main(string[] args)
+         {
+            Random rnd = new Random();
 
-            public void GetName()
-            
-            {
-                Console.WriteLine($"salve sono {Nome} ");
-            }
-            public void GetCognome()
-            {
-                Console.WriteLine($" il mio cognome è {Cognome}");
-            }
-
-            public void GetEta()
-            {
-                Console.WriteLine($"la mia età è di {Eta}");
-            }
-
-            public void GetDettagli()
-            {
-                Console.WriteLine($"sono molto contento di conoscervi sono {Nome} {Cognome} e ho {Eta} anni ");
-            }
-        }
-        static void Main(string[] args)
-        {
-            Persona bella = new Persona();
-            bella.Nome = "Giulia";
-            bella.Cognome = "Sapo";
-            bella.Eta = 25;
-
-            bella.GetName();
-            bella.GetCognome();
-            bella.GetEta();
-            bella.GetDettagli();
-
-            
-        }
+            ArrayList persone = new ArrayList() { "Riccardo", "viviana", "gianmarco", "mirko", "aiello", "andrea", "alan", "godyl", "giulia" };
+    
+            int personeIndex = rnd.Next(persone.Count);
+ 
+            Console.WriteLine(persone[personeIndex]);
+         }
     }
 }
+
