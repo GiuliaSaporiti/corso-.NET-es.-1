b435f14627f3f2794c34834d65a3c7323d5587f7
GiuliaSaporiti <127093059+GiuliaSaporiti@users.noreply.github.com>
2023-03-18 13:08:38 +0100

    Add files via upload

--- ex7.cs
+++ ex7.cs
@@ -7,99 +7,111 @@ namespace esercizio7
 
     class Contribuente
     {
-        public  string Nome { get; private set; }
-        public  string Cognome { get; private set; }
+        public string Nome { get; private set; }
+        public string Cognome { get; private set; }
         public string DataNascita { get; private set; }
         public string CodiceFiscale { get; private set; }
         public string Sesso { get; private set; }
         public string ComuneResidenza { get; private set; }
         public decimal RedditoAnnuale { get; private set; }
         public decimal Aliquota { get; private set; }
+        
 
-        public void CalAliquota()
+        public decimal CalAliquota()
         {
 
             if (RedditoAnnuale > 0 && RedditoAnnuale <= 15000)
 
             {
-                Aliquota = (RedditoAnnuale) * (23 / 100);
+               Aliquota = (RedditoAnnuale) * (23 / 100);
             }
-            else if (RedditoAnnuale > 15001 && RedditoAnnuale <= 28000)
+            else if (RedditoAnnuale > 15000 && RedditoAnnuale <= 28000)
             {
-                Aliquota = (RedditoAnnuale - 3450) * (27 / 100);
+                Aliquota = 3450 + (RedditoAnnuale - 15000) * (27 / 100);
             }
             else if (RedditoAnnuale > 28000 && RedditoAnnuale <= 55000)
             {
-                Aliquota = (RedditoAnnuale - 6960) * (38 / 100);
+               Aliquota = 6960 + (RedditoAnnuale - 28000) * (38 / 100);
             }
-            else if (RedditoAnnuale > 55001 && RedditoAnnuale <= 75000) 
+            else if (RedditoAnnuale > 55000 && RedditoAnnuale <= 75000)
 
             {
-                Aliquota = (RedditoAnnuale - 17220) * (41/100);
+                Aliquota = 17220 + (RedditoAnnuale - 28000) * (41 / 100);
             }
-            else if (RedditoAnnuale >= 75001)
+            else if (RedditoAnnuale >= 75000)
             {
-                Aliquota = (RedditoAnnuale -25420) * (43/100);
+                Aliquota = 25420 + (RedditoAnnuale - 75000) * (43 / 100);
             }
-
+            return Aliquota;
         }
 
-        public void Persona()
+        
+        public Contribuente (string nome, string cognome, string datanascita, string codicefiscale, string sesso, string comuneresidenza, decimal redditoAnnuale )
+        {
+            Nome = nome;
+            Cognome = cognome;
+            DataNascita = datanascita;
+            CodiceFiscale = codicefiscale;
+            Sesso = sesso;
+            ComuneResidenza = comuneresidenza;
+            RedditoAnnuale = redditoAnnuale;
+        }
+    }
+
+    class Program
+    {
+        public static Contribuente Persona()
         {
             Console.WriteLine("come ti chiami ?");
             string nome1 = Console.ReadLine();
-            Nome = nome1;
+            
 
             Console.WriteLine("cognome ?");
             string cognome1 = Console.ReadLine();
-            Cognome = cognome1;
+           
 
             Console.WriteLine("data di nascita ?");
             string datanascita1 = Console.ReadLine();
-            DataNascita = datanascita1;
+            
 
             Console.WriteLine("codice fiscale ?");
             string codicefiscale1 = Console.ReadLine();
-            CodiceFiscale = codicefiscale1;
+            
 
             Console.WriteLine("inserisci il tuo sesso");
             string sesso1 = Console.ReadLine();
-            Sesso = sesso1;
+            
 
             Console.WriteLine("quale è il comune di residenza");
             string comuneresidenza = Console.ReadLine();
-            ComuneResidenza = comuneresidenza;
+            
 
             Console.WriteLine("inserisci reddito annuale");
             string redditoannuale = Console.ReadLine();
             decimal redditoannuale1 = decimal.Parse(redditoannuale);
-            RedditoAnnuale = redditoannuale1;
-
-            Console.WriteLine("inserisci aliquota");
-            string aliquota = Console.ReadLine();
-            decimal aliquota1 = decimal.Parse(aliquota);
-            Aliquota = aliquota1;
-
-            Console.WriteLine($"contribuente {Nome} {Cognome} ");
-            Console.WriteLine($"sono nato il giorno {DataNascita}");
-            Console.WriteLine($"sono residente a {ComuneResidenza}");
-            Console.WriteLine($"il mio codice fiscale è {CodiceFiscale}");
-            Console.WriteLine($"sono un {Sesso}");
-            Console.WriteLine($"il mio reddito annuale è di {RedditoAnnuale}");
-            Console.WriteLine($"aliquota {Aliquota}");
+                
+            return new Contribuente(nome1, cognome1, datanascita1, codicefiscale1, sesso1, comuneresidenza, redditoannuale1);
+            
         }
+       
+        public static void Stampa(Contribuente x)
 
-    }
+        {
+            x.CalAliquota();
+            Console.WriteLine($"contribuente {x.Nome} {x.Cognome} ");
+            Console.WriteLine($"sono nato il giorno {x.DataNascita}");
+            Console.WriteLine($"sono residente a {x.ComuneResidenza}");
+            Console.WriteLine($"il mio codice fiscale è {x.CodiceFiscale}");
+            Console.WriteLine($"sono un {x.Sesso}");
+            Console.WriteLine($"il mio reddito annuale è di {x.RedditoAnnuale}");
+            Console.WriteLine($"aliquota {x.Aliquota}");
 
-}
-
-    internal class Program
-    {
+        }
 
         static void Main(string[] args)
         {
-            Contribuente cicciolo = new Contribuente();
-            cicciolo.Persona(); 
+            Stampa(Persona());
         }
     }
 
+}
